ea5c33bd22f03111fe885a4eebdcf462ba8416b4
JTG94 <sky_engel@naver.com>
2020-02-17 22:42:21 +0900

    news와 ask api 코드 추가

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,12 +1,27 @@
 <template>
-    <div>
-        ask
-    </div>
+  <div>
+    <div v-for="item in ask">{{ item.title }}</div>
+  </div>
 </template>
 
 <script>
+import { fetchAskList } from '../api/index.js'
 export default {
-
+  data() {
+    return {
+      ask: []
+    }
+  },
+  //컴포넌트가 생성되자 마자 필요한 로직이 들어가는곳 (라이플 사이클 훅), beforeMount, mounted 등이 있음
+  created() {
+    var vm = this
+    fetchAskList()  // return new Promise() 가 생성되어 .then() .catch() 가능 - 비동기
+    .then(function(response) {
+      vm.ask = response.data;
+    })
+    .catch(error => console.log(error))
+  },
+  
 }
 </script>
 
--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-      jobs
+      <div v-for="job in jobs">{{ job.title }}</div>
   </div>
 </template>
 
 <script>
+import { fetchJobsList } from '../api/index.js'
 export default {
-
+  data() {
+    return {
+      jobs: []
+    }
+  },
+  created() {
+    fetchJobsList()
+    .then(response => this.jobs = response.data) // 화살표 표기로 바꾸면 this 바인딩을 그냥 this 해주면 되기떄문에 번거로움이 사라짐
+    .catch(error => console.log(error))
+  }
 }
 </script>
 
